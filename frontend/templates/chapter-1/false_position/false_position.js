8f5e3a327ad5beb89d9a8d6840bcaab979df5082
Pradita777 <andres123prada@gmail.com>
2023-11-14 01:04:48 -0500

    Agrege Nuevos html para las funciones del capitulo 1

--- frontend/templates/chapter-1/false_position/false_position.js
+++ frontend/templates/chapter-1/false_position/false_position.js
@@ -6,7 +6,7 @@ function submitForm(event) {
 
     // Crear un objeto que contenga los datos
     var data = {};
-    formData.forEach(function(value, key) {
+    formData.forEach(function (value, key) {
         // Convertir los valores a cadenas
         data[key] = value.toString();
     });
@@ -19,11 +19,44 @@ function submitForm(event) {
         },
         body: JSON.stringify(data)
     })
-    .then(response => response.json())  // Parsear la respuesta JSON del servidor
-    .then(data => {
-        // Actualizar el contenido de #result-container con la respuesta del servidor
-        console.log(data)
-        document.getElementById("false_positions_results").textContent = data.result;
-    })
-    .catch(error => console.error("Error:", error));
+        .then(response => response.json())  // Parsear la respuesta JSON del servidor
+        .then(data => {
+            // Crear y agregar la tabla con los resultados al elemento con el id "false_positions_results"
+            var table = createTable(data);
+            document.getElementById("false_positions_results").innerHTML = "";
+            document.getElementById("false_positions_results").appendChild(table);
+        })
+        .catch(error => console.error("Error:", error));
+}
+
+// Función para crear la tabla
+function createTable(data) {
+    var table = document.createElement("table");
+    table.classList.add("result-table"); // Puedes agregar una clase para darle estilo si es necesario
+
+    // Crear encabezados de tabla
+    var thead = document.createElement("thead");
+    var headerRow = document.createElement("tr");
+    for (var key in data[0]) {
+        var th = document.createElement("th");
+        th.textContent = key;
+        headerRow.appendChild(th);
+    }
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Crear cuerpo de tabla
+    var tbody = document.createElement("tbody");
+    data.forEach(function (item) {
+        var row = document.createElement("tr");
+        for (var key in item) {
+            var cell = document.createElement("td");
+            cell.textContent = item[key];
+            row.appendChild(cell);
+        }
+        tbody.appendChild(row);
+    });
+    table.appendChild(tbody);
+
+    return table;
 }
